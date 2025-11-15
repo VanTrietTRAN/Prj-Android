79cd8292662ad67fb27bdaa4eab301a5720a8c67
VanTrietTRAN <work.triettran@gmail.com>
2025-11-15 07:57:31 +0700

    Deploy-API

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,24 +1,24 @@
-import {neon}  from '@neondatabase/serverless'
+import { neon } from "@neondatabase/serverless";
 
 import "dotenv/config";
 
-// Tạo kết nối SQL bằng cách sử dụng DB URL từ biến môi trường
+// Creates a SQL connection using our DB URL
 export const sql = neon(process.env.DATABASE_URL);
 
-export async function initDB(){
-    try{
-        await sql `CREATE TABLE IF NOT EXISTS transaction(
-            id SERIAL PRIMARY KEY,
-            user_id VARCHAR(255) NOT NULL,
-            title VARCHAR(255) NOT NULL,
-            amount DECIMAL(10,2) NOT NULL,
-            category VARCHAR(255) NOT NULL,
-            created_at DATE NOT NULL DEFAULT CURRENT_DATE
-        )`;
-        console.log("Database initialized successfully");
+export async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title  VARCHAR(255) NOT NULL,
+      amount  DECIMAL(10,2) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
 
-    } catch(error){
-        console.error("Error initializing database:", error);
-        process.exit(1); // 1 là faileure, 0 là success
-    }
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initializing DB", error);
+    process.exit(1); // status code 1 means failure, 0 success
+  }
 }
